15aaa8b22cd19736f6690f4b9ecc37067c1dd44f
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2022-04-20 16:32:47 -0300

    feat: implemented new test for creating new user

--- cypress/integration/api-tests/user.spec.js
+++ cypress/integration/api-tests/user.spec.js
@@ -1,9 +1,17 @@
+import faker from '@faker-js/faker';
+
+let userData = {
+    name: faker.name.firstName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+}
+
 describe('CTAA api testing', () => {
-    it('fetches users - GET', () => {
-        cy.request('users/').as('userRequest');
-        cy.get('@userRequest').then(users => {
-            expect(users.status).to.eq(200);
-            assert.isArray(users.body, []);
+    it.only('POST /users', () => {
+        cy.request('POST', '/users', userData).then(response => {
+            expect(response.status).to.eq(201);
+            expect(response.body).to.have.property('name', userData.name);
+            expect(response.body).to.have.property('email', userData.email);
         });
     });
 });
---
+++ cypress/support/utils.js
@@ -0,0 +1,33 @@
+export function generatedRandomString(length) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz  ';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
+
+export function generatedRandomInvalidString(length) {
+    let result = '';
+    let characters = '!@#$%¨&*()@##$¹²³£¹²³£¬';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
+
+
+export function generatedEmailAddress(length) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result + "@email.com";
+}
