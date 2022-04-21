7ca6799e2d7b01ff1123a642353b9a00ab79ca52
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2022-04-21 14:44:43 -0300

    refactor: replace data for use fixtures

--- cypress/integration/api-tests/user.spec.js
+++ cypress/integration/api-tests/user.spec.js
@@ -1,17 +1,30 @@
 import faker from '@faker-js/faker';
-
-let userData = {
-    name: faker.name.firstName(),
-    email: faker.internet.email(),
-    password: faker.internet.password()
-}
+import { userValid, userWithoutName } from '../../fixtures/user.json';
 
 describe('CTAA api testing', () => {
-    it.only('POST /users', () => {
-        cy.request('POST', '/users', userData).then(response => {
+    it('POST /users - Creating user sucess', () => {
+
+        userValid.name = faker.fake.name;
+        userValid.email = faker.internet.email();
+        userValid.password = faker.internet.password();
+
+        cy.request('POST', '/users', userValid).then(response => {
             expect(response.status).to.eq(201);
-            expect(response.body).to.have.property('name', userData.name);
-            expect(response.body).to.have.property('email', userData.email);
+            expect(response.body).to.have.property('name', userValid.name);
+            expect(response.body).to.have.property('email', userValid.email);
+        });
+    });
+
+    it.only('POST /users - Creating user without name', () => {
+        cy.request({
+            method: 'POST',
+            url: '/users',
+            failOnStatusCode: false,
+            body: userWithoutName
+        }).then(res => {
+            expect(res).to.have.property('status', 400);
+            expect(res.body).to.contains.property('erros');
+            expect(res.body.erros).to.contains('Campo nome é obrigatório');
         });
     });
 });
