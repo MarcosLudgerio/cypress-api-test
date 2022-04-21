52b723b7fec67f9fe2f5d415c5165122256d4a14
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2022-04-21 15:26:48 -0300

    feat: implemented authentication tests

---
+++ cypress/integration/api-tests/authentication.spec.js
@@ -0,0 +1,53 @@
+import { userLogin } from '../../fixtures/user.json';
+
+describe('CTAA api testing', () => {
+    it('POST /auth/login - Login with sucessfull', () => {
+        cy.request('POST', '/auth/login', userLogin).then(response => {
+            expect(response.status).to.eq(200);
+        });
+    });
+
+    it('POST /auth/login - Login with password invalid', () => {
+        userLogin.password = "123456";
+        cy.request({
+            method: 'POST',
+            url: '/auth/login',
+            failOnStatusCode: false,
+            body: userLogin
+        }).then(response => {
+            expect(response.status).to.eq(401);
+            expect(response.body).to.contains.property('erros');
+            expect(response.body.erros).to.contains('Falha ao tentar efetuar o login, verifique os dados e tente novamente');
+        });
+    });
+
+    it('POST /auth/login - Login with email not valid', () => {
+        userLogin.email = "notvalid";
+        cy.request({
+            method: 'POST',
+            url: '/auth/login',
+            failOnStatusCode: false,
+            body: userLogin
+        }).then(response => {
+            expect(response.status).to.eq(400);
+            expect(response.body).to.contains.property('erros');
+            expect(response.body.erros).to.contains('O email precisa ser válido');
+        });
+    });
+
+    it('POST /auth/login - Login with null fields', () => {
+        userLogin.email = "";
+        userLogin.password = "";
+        cy.request({
+            method: 'POST',
+            url: '/auth/login',
+            failOnStatusCode: false,
+            body: userLogin
+        }).then(response => {
+            expect(response.status).to.eq(400);
+            expect(response.body).to.contains.property('erros');
+            expect(response.body.erros).to.contains('Campo email é obrigatório');
+            expect(response.body.erros).to.contains('Campo senha é obrigatório');
+        });
+    });
+});
--- cypress/integration/api-tests/user.spec.js
+++ cypress/integration/api-tests/user.spec.js
@@ -1,30 +1,86 @@
 import faker from '@faker-js/faker';
-import { userValid, userWithoutName } from '../../fixtures/user.json';
+import { userValid, userBlank, userInvalidEmail, userPasswordLassThenAllowed, userPasswordMostThenAllowed, userDuplicatedEmail } from '../../fixtures/user.json';
 
 describe('CTAA api testing', () => {
-    it('POST /users - Creating user sucess', () => {
+    context("Creating users", () => {
+        it('POST /users - Creating user sucess', () => {
 
-        userValid.name = faker.fake.name;
-        userValid.email = faker.internet.email();
-        userValid.password = faker.internet.password();
+            userValid.name = faker.fake.name;
+            userValid.email = faker.internet.email();
+            userValid.password = faker.internet.password();
 
-        cy.request('POST', '/users', userValid).then(response => {
-            expect(response.status).to.eq(201);
-            expect(response.body).to.have.property('name', userValid.name);
-            expect(response.body).to.have.property('email', userValid.email);
+            cy.request('POST', '/users', userValid).then(response => {
+                expect(response.status).to.eq(201);
+                expect(response.body).to.have.property('name', userValid.name);
+                expect(response.body).to.have.property('email', userValid.email);
+            });
         });
-    });
 
-    it.only('POST /users - Creating user without name', () => {
-        cy.request({
-            method: 'POST',
-            url: '/users',
-            failOnStatusCode: false,
-            body: userWithoutName
-        }).then(res => {
-            expect(res).to.have.property('status', 400);
-            expect(res.body).to.contains.property('erros');
-            expect(res.body.erros).to.contains('Campo nome é obrigatório');
+        it('POST /users - Creating user with blank datas', () => {
+            cy.request({
+                method: 'POST',
+                url: '/users',
+                failOnStatusCode: false,
+                body: userBlank
+            }).then(res => {
+                expect(res).to.have.property('status', 400);
+                expect(res.body).to.contains.property('erros');
+                expect(res.body.erros).to.contains('Campo nome é obrigatório');
+                expect(res.body.erros).to.contains('Campo senha é obrigatório');
+                expect(res.body.erros).to.contains('Campo email é obrigatório');
+            });
+        });
+
+        it('POST /users - Creating user with invalid email address', () => {
+            cy.request({
+                method: 'POST',
+                url: '/users',
+                failOnStatusCode: false,
+                body: userInvalidEmail
+            }).then(res => {
+                expect(res).to.have.property('status', 400);
+                expect(res.body).to.contains.property('erros');
+                expect(res.body.erros).to.contains('Email inválido');
+            });
+        });
+
+        it('POST /users - Creating user with password less ', () => {
+            cy.request({
+                method: 'POST',
+                url: '/users',
+                failOnStatusCode: false,
+                body: userPasswordLassThenAllowed
+            }).then(res => {
+                expect(res).to.have.property('status', 400);
+                expect(res.body).to.contains.property('erros');
+                expect(res.body.erros).to.contains('A senha deve ter entre 8 e 64 caracteres');
+            });
+        });
+
+        it('POST /users - Creating user with password more', () => {
+            cy.request({
+                method: 'POST',
+                url: '/users',
+                failOnStatusCode: false,
+                body: userPasswordMostThenAllowed
+            }).then(res => {
+                expect(res).to.have.property('status', 400);
+                expect(res.body).to.contains.property('erros');
+                expect(res.body.erros).to.contains('A senha deve ter entre 8 e 64 caracteres');
+            });
+        });
+
+        it('POST /users - Creating user with duplicated email address', () => {
+            cy.request({
+                method: 'POST',
+                url: '/users',
+                failOnStatusCode: false,
+                body: userDuplicatedEmail
+            }).then(res => {
+                expect(res).to.have.property('status', 409);
+                expect(res.body).to.contains.property('erros');
+                expect(res.body.erros).to.contains('Usuário com este email ja foi cadastrado');
+            });
         });
     });
 });
